8eb358935ea26b7ec8622acc878a81f9fdeafd3a
LuisMDev <luisdev002@gmail.com>
2025-05-04 01:24:10 -0400

    10

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
-import node from "@astrojs/node";
-import clerk from "@clerk/astro";
+// import node from "@astrojs/node";
+// import clerk from "@clerk/astro";
 
 // https://astro.build/config
 export default defineConfig({
